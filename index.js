f13ebfc4748354b421418e430c0f995ea3cc49b2
dangwin <danghwin@gmail.com>
2020-12-01 19:39:21 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(array, search) {
+
+    return array.filter( s => s.toLowerCase() === search.toLowerCase())
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(s => s.slice(0,string.length).toLowerCase() === string.toLowerCase())
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(s => s.name === string)
+}
